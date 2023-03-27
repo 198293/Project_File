7053c9445b74f959339886fd682c787daa8c6daf
198293 <rlagustj0411@g.hongik.ac.kr>
2023-03-28 01:27:15 +0900

    Initial commit

---
+++ Project-File/src/main/java/Project/File/Project/File/ProjectFileApplication.java
@@ -0,0 +1,13 @@
+package Project.File.Project.File;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFileApplication.class, args);
+	}
+
+}
---
+++ Project-File/src/test/java/Project/File/Project/File/ProjectFileApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.File.Project.File;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
